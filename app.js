e437525146f5cf4f3c01ab83c7429d4ff7ba5b54
Victoria Moor <victimofuroboros@gmail.com>
2023-06-20 21:47:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function app() {
+        var message = document.getElementById("text").value;
+        document.getElementById("message1").innerText = message;
+        document.getElementById("text").value = '';
+        //console.log(message)
+}
